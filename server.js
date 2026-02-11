5c0312df9659529d9859d28b1fa49097cd739e7d
kreislers169-svg <kreislers169@gmail.com>
2026-02-11 23:46:45 +0100

    Update server.js to use environment variable for PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); 
@@ -81,3 +81,4 @@ app.listen(PORT, () => {
     console.log(`🚀 Serveris griežas uz http://localhost:${PORT}`);
 
 });
+
